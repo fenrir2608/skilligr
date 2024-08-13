263b0840080f15e923d71b44743ad86716647da3
devarshichinivar <dbchinivar15@gmail.com>
2024-08-13 15:22:07 +0530

    Updated controllers

---
+++ backend/controllers/learningresourcesController.js
@@ -0,0 +1,17 @@
+import conn from "../helpers/connection.js"
+
+export const createResource = (req, res) => {
+    // TODO: Implement create resource logic
+};
+
+export const viewAllResource = (req, res) => {
+    // TODO: Implement view resources logic
+};
+
+export const viewResource = (req, res) => {
+    // TODO: Implement view resource logic
+};
+
+export const deleteResource = (req, res) => {
+    // TODO: Implement delete resource logic
+};
---
+++ backend/controllers/notificationController.js
@@ -0,0 +1,21 @@
+import conn from "../helpers/connection.js"
+
+export const createNotification = (req, res) => {
+    // TODO: Implement create notification logic
+};
+
+export const viewAllNotification = (req, res) => {
+    // TODO: Implement view notifications logic
+};
+
+export const viewNotification = (req, res) => {
+    // TODO: Implement view notification logic
+};
+
+export const updateNotification = (req, res) => {
+    // TODO: Implement update notification logic
+};
+
+export const deleteNotification = (req, res) => {
+    // TODO: Implement delete notification logic
+};
