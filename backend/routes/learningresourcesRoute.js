128bb7293e4074e6d64444e9d59a2b00c14b8641
AtmanChavda <atmanchavda@gmail.com>
2024-08-13 16:06:57 +0530

    routes commited

---
+++ backend/routes/learningresourcesRoute.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import {
+    createResource,
+    viewAllResource,
+    viewResource,
+    deleteResource
+} from '../controllers/learningresourcesController.js';
+
+const router = express.Router();
+
+// Route to create a resource
+router.post('/create', createResource);
+
+// Route to view all resources
+router.get('/viewAll', viewAllResource);
+
+// Route to view a specific resource by ID
+router.get('/view/:id', viewResource);
+
+// Route to delete a resource by ID
+router.delete('/delete/:id', deleteResource);
+
+export default router;
