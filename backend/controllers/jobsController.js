a762fae7e972cc1800d589c1a1ad2ee7314ab10f
devarshichinivar <dbchinivar15@gmail.com>
2024-09-13 11:47:53 +0530

    Add-job

--- backend/controllers/jobsController.js
+++ backend/controllers/jobsController.js
@@ -1,7 +1,42 @@
 import conn from "../helpers/connection.js"
 
-export const createJob = (req, res) => {
-    // TODO: Implement create job logic
+export const createJob = async (req, res) => {
+    try {
+        const { job_title, description, created_by, company_name, company_profile, registration_link, dept, semester, deadline} = req.body;
+
+        const [adminUser] = await conn.query(`
+            SELECT id FROM users WHERE id = ? AND role = 'admin'
+          `, [created_by]);
+    
+          if (adminUser.length === 0) {
+            return res.status(400).send("Invalid created_by ID");
+          }
+        
+        const currentTimestamp = Math.floor(Date.now() / 1000);
+
+        if (new Date(deadline).getTime() / 1000 < currentTimestamp) {
+            return res.status(400).send("Deadline cannot be set before the current time.");
+        }
+          
+        const deadlineTimestamp = new Date(deadline).getTime() / 1000;
+
+        if (isNaN(deadlineTimestamp)) {
+            return res.status(400).send("Invalid deadline format. Please provide a valid timestamp.");
+        }
+
+          const [result] = await conn.query(
+            `
+            INSERT INTO jobs (job_title, description, created_by, company_name, company_profile, registration_link, dept, semester, deadline)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+            `,
+            [job_title, description, created_by, company_name, company_profile, registration_link, dept, semester, deadline]
+          );
+
+          res.status(201).send("Job posted successfully");
+    } catch (error) {
+        console.error("error: ", error);
+        res.status(500).send("Failed to create Job posting.");
+    }
 };
 
 export const viewAllJob = (req, res) => {
