57b0e5d0cdcaa1b0a373b29906d508fdc93d3b26
devarshichinivar <dbchinivar15@gmail.com>
2024-10-02 17:04:57 +0530

    Notification module completed alongwith user endpoints

--- backend/routes/notificationRoute.js
+++ backend/routes/notificationRoute.js
@@ -5,23 +5,22 @@ import {
     viewNotification,
     updateNotification,
     deleteNotification,
+    getNotificationsUser,
     getNotificationUser
 } from '../controllers/notificationController.js';
 
-import { verifyAdmin } from "../middlewares/authMiddleware.js";
+import { verifyAdmin, verify } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
 //User Routes
-router.get('/getNotification', getNotificationUser);
+router.get('/getNotifications',verify, getNotificationsUser);
+router.get('/getNotification/:id',verify, getNotificationUser);
 // Admin Routes
-router.use(verifyAdmin);
-router.post('/create', createNotification);
-router.get('/viewAll', viewAllNotification);
-router.get('/view/:id', viewNotification);
-router.put('/update/:id', updateNotification);
-router.delete('/delete/:id', deleteNotification);
-
-
+router.post('/create',verifyAdmin, createNotification);
+router.get('/viewAll',verifyAdmin, viewAllNotification);
+router.get('/view/:id',verifyAdmin, viewNotification);
+router.put('/update/:id',verifyAdmin, updateNotification);
+router.delete('/delete/:id',verifyAdmin, deleteNotification);
 
 export default router;
